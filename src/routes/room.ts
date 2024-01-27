7ba3f5b170e5c9bcd8c96a7575b45565121c9415
AbVicario <abelvicariogarcia@gmail.com>
2024-01-27 23:13:12 +0100

    crear crud room

--- src/routes/room.ts
+++ src/routes/room.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono'
 import { Answer } from '../models/answer'
-import { saveRoom } from '../controllers/room/admin/room'
+import { getOneRoom, saveRoom, getAllConfirmed } from '../controllers/room/room'
+import { deleteRoom } from '../controllers/room/room'
+import { updateRoom } from '../controllers/room/room'
+import { number } from 'zod'
 
 const app = new Hono()
 
@@ -9,4 +12,29 @@ app.post('/insert', async (c) => {
     return c.json({ data: result.data, ok: result.ok }, result.status)
 })
 
+app.delete('/deleteOne/:number',async (c) => {
+    console.log(c.req.param('number'));
+    const result = await deleteRoom(parseInt(c.req.param('number')))
+    return c.json({ data: result.data, ok: result.ok }, result.status)
+    
+})
+
+app.put('/updateOne/:number',async (c) => {
+    const result = await updateRoom(c, parseInt(c.req.param('number')))
+    return c.json({ data: result.data, ok: result.ok }, result.status)
+})
+
+app.get('/getOne/:number',async (c) => {
+    const result = await getOneRoom(parseInt(c.req.param('number')))
+    return c.json({ data: result.data, ok: result.ok }, result.status)
+})
+
+app.get('/getAllConfirmed/:confirmed',async (c) => {
+    const result = await getAllConfirmed(c.req.param('confirmed'))
+    return c.json({ data: result.data, ok: result.ok }, result.status)
+})
+app.get('/getAllCancelled/:cancelled',async (c) => {
+    const result = await getAllConfirmed(c.req.param('cancelled'))
+    return c.json({ data: result.data, ok: result.ok }, result.status)
+})
 export default app
