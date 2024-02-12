b45992f1052adaf28e28fc0da25d05073d1f7742
BluewyWork <125402892+BluewyWork@users.noreply.github.com>
2024-02-12 23:54:21 +0100

    Habitaciones (#21)
    fix: make update guest route worky worky
    
    ---------
    
    Co-authored-by: AbVicario <152438341+AbVicario@users.noreply.github.com>

--- src/routes/guestBook.ts
+++ src/routes/guestBook.ts
@@ -2,6 +2,8 @@ import { Hono } from 'hono'
 import { guestShowRoomsDate } from '../controllers/room/guestShowRoomsDate'
 import { guestBookRoom } from '../controllers/book/guestBookRoom'
 
+
+
 const app = new Hono()
 
 app.put('/book/:number', async (c) => {
@@ -18,14 +20,14 @@ app.put('/cancel/:number', async (c) => {
 })
 
 app.get('/search', async (c) => {
-    var result = {
-        data: '',
+    var result={
+        data:'',
         ok: false,
-        status: 505,
+        status: 505
     }
     const checkIn = c.req.query('checkIn')
     const checkOut = c.req.query('checkOut')
-    if (checkIn && checkOut) {
+    if(checkIn && checkOut){
         result = await guestShowRoomsDate(checkIn, checkOut)
     }
     return c.json({ data: result.data, ok: result.ok }, result.status)
