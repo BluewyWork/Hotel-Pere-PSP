098c2a3af938d3dcd33a3e74ed9a29ca9689c7e2
AbVicario <152438341+AbVicario@users.noreply.github.com>
2024-02-16 08:36:30 +0100

    modificar controlador para validar imagen

--- src/controllers/tableGuest/guestShowMyself.ts
+++ src/controllers/tableGuest/guestShowMyself.ts
@@ -4,6 +4,10 @@ import { Context } from 'hono'
 export const guestShowMyself = async (c: Context): Promise<Answer> => {
     const guest = c.get('jwtPayload')
 
+    if (!guest.hasOwnProperty('image')) {
+        guest.image = ''
+    }
+
     return {
         data: guest,
         status: 200,
